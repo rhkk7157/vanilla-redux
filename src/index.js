407c619de0dacd48520495feaae0f7c5de78ed61
hyeri <rhkk7157@gmail.com>
2021-07-31 12:18:36 +0900

    changed react project, store and components connect()

--- src/index.js
+++ src/index.js
@@ -1,76 +1,12 @@
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./store";
 
-const form =  document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const addToDo = (text) => {
-  return { 
-    type: ADD_TODO, 
-    text 
-  };
-};
-
-const deleteToDo = (id) => {
-  return { 
-    type: DELETE_TODO,
-    id 
-  };
-};
-
-const reducer = (state=[], action) => {
-  switch(action.type) {
-    case ADD_TODO :
-      const newToDoObj = { text: action.text, id: Date.now() };
-      return [newToDoObj, ...state];
-      // return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE_TODO :
-      const cleaned = state.filter(toDo => toDo.id !== action.id);
-      return cleaned;
-      // return state.filter(toDo => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-store.subscribe(() => console.log(store.getState()));
-
-
-const dispatchDeleteToDo = (e) => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-}
-
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";  // ul 안에 있는 HTML 태그 비움. li  가 있을테니까
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  })
-}
-
-store.subscribe(paintToDos);
-
-const dispatchAddToDo = (text) => {
-  store.dispatch(addToDo(text));
-}
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddToDo(toDo);
-}
-
-form.addEventListener("submit", onSubmit);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById("root")
+);
